e74239f3e7195db44b2fc7e09969780673f8675b
Cokane415 <catherineaokane@gmail.com>
2019-06-21 13:54:44 -0700

    Set up  parent component

---
+++ client/src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import $ from 'jquery';
+
+
+class WineTracker extends React.Component {
+    constructor(props) {
+        super(props)
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Wine Tracker</h1>
+            </div>
+        )
+    }
+}
+
+export default WineTracker;
